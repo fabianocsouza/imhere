e332b5ef652698999db507c3937fd311b5b73d52
Fabiano C. Souza <fabiano1403@hotmail.com>
2025-04-12 13:28:24 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+import { styles } from './styles';
+
+export function Home() {
+  function handleParticipantAdd(){
+    console.log("Você clicou no botão de adicionar!");
+    
+  }
+  return (
+    <View style={styles.container}>
+          <Text style={styles.eventName}>
+            Nome do evento
+          </Text>
+    
+          <Text style={styles.eventDate}>
+            Sábado, 12 de Abril de 2025.
+          </Text>
+
+          <View style={styles.form}>
+            <TextInput 
+              style={styles.input}
+              placeholder='Nome do participante'
+              placeholderTextColor='#6B6B6B'
+            />
+            <TouchableOpacity 
+              style={styles.button}
+              onPress={handleParticipantAdd}
+            >
+              <Text style={styles.buttonText}>
+                +
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+  );
+}
